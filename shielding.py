cf04abaa59c20c391f02a456c1e0675f59e6b76f
Aaron Curtis <foobarbecue@gmail.com>
2016-04-10 18:42:20 -0600

    Read sample locations

--- shielding.py
+++ shielding.py
@@ -1,19 +1,22 @@
+import csv
 import bpy
 from mathutils.bvhtree import BVHTree  
 from mathutils import Vector
 origin = Vector((0,0,0))
 
 class shielding_scene:
-    def __init__(self, filepath, samples=None):
+    def __init__(self, filepath, samples_filepath):
         bpy.ops.import_mesh.stl(filepath = filepath)
         self.mesh = bpy.context.object
         #BVH tree is a technique for speeding up the ray - mesh intersections
         self.tree = BVHTree.FromObject(self.mesh, bpy.context.scene)
+        self.samples = []
         self.intersections = []
         self.bounding_sphere = None
-    def intersect_cosrays(self, n=3000):
+        self.read_samples(self, samples_filepath)
+    def intersect_cosrays(self, n=3000, sample_loc=origin):
         if not self.bounding_sphere:
-            #Make an sphere twice the diameter of the mesh, with n vertices.
+            #Make a sphere twice the diameter of the mesh, with n vertices.
             #The number of vertices is 10*4**n+2, so need log((n-2)/10,4) subdivs to make them.
             #The sphere constructor will round this to the nearest number of subdivisions.
             bpy.ops.mesh.primitive_ico_sphere_add(
@@ -24,8 +27,14 @@ class shielding_scene:
             self.bounding_sphere=bpy.context.object
         for vert in self.bounding_sphere.data.vertices:
             self.intersections.append(get_ray_mesh_intersections(self.mesh, vert.co))
-        
-def get_ray_mesh_intersections(rock_mesh, ray_source):
+    def read_samples(self, samples_filepath=None):
+        samples_filepath = samples_filepath or self.samples_filepath
+        with open(samples_filepath) as samplesfile:
+            rdr = csv.reader(samplesfile, quoting=csv.QUOTE_NONNUMERIC)
+            for line in rdr:
+                self.samples.append(Vector(line))
+
+def get_ray_mesh_intersections(rock_mesh, ray_source, sample_loc):
     #It seems the the first argument of ray_cast must be inside the object, i.e. the rays have to go outwards.
     intersections = [origin,]
     hit, intersection_pt, face_normal, face_ind = rock_mesh.ray_cast(origin, ray_source)
@@ -46,5 +55,7 @@ def get_lengths_from_intersections(intersections):
         rock_segment_lengths = [(seg[0]-seg[1]).length for seg in zip(evens, odds)]
         lengths.append(sum(rock_segment_lengths))
     return lengths
-    
-pbrs = shielding_scene(filepath = r"D:\aaron\sfm\parmelee_cosmogenic\shielding\src\gv01_everything_cntrd.stl")
+
+#def get_shielding_factor_from_lengths(lengths):
+pbrs = shielding_scene(filepath = r"D:\aaron\sfm\parmelee_cosmogenic\shielding\src\gv01_everything_cntrd.stl",
+    samples_filepath = r"D:\aaron\sfm\parmelee_cosmogenic\shielding\src\gv2_samples.csv")
